5ae381ef1dde7ced14a6282f65e1e4935259ca27
Matt Holton <mtholton@valdosta.edu>
2022-02-21 23:24:06 -0500

    getting everything from class set up on github

---
+++ src/components/user_detail.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const UserDetail = ({ user }) => {
+	if (!user) {
+		return <div>Loading...</div>;
+	}
+
+	return (
+		<div>
+			<div>
+				<img src={user.picture.large} alt="" />
+			</div>
+		</div>
+	);
+};
+
+export default UserDetail;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import SearchBar from "./components/search_bar";
+import UserList from "./components/user_list";
+import UserDetail from "./components/user_detail";
+import reportWebVitals from "./reportWebVitals";
+
+class App extends Component {
+	getFakeMembers = (count, nat) =>
+		new Promise((resolves, rejects) => {
+			const api = `https://api.randomuser.me/?nat=${nat}&results=${count}`;
+			const request = new XMLHttpRequest();
+			request.open("GET", api);
+			request.onload = () =>
+				request.status === 200
+					? resolves(JSON.parse(request.response).results)
+					: rejects(Error(request.statusText));
+			request.onerror = (err) => rejects(err);
+			request.send();
+		});
+
+	userSearch(count, nat) {
+		this.getFakeMembers(count, nat).then(
+			(users) => this.setState({ users, selectedNat: nat, selectedUser: 0 }),
+			(err) => console.log(new Error("failed"))
+		);
+	}
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			users: [],
+			selectedNat: "",
+			selectedUser: "",
+		};
+
+		this.userSearch(10, "US");
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar onSearchTermChange={(term) => this.userSearch(10, term)} />
+				<UserList
+					users={this.state.users}
+					onUserSelect={(selectedUser) => this.setState({ selectedUser })}
+				/>
+				<UserDetail user={this.state.selectedUser} />
+			</div>
+		);
+	}
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
