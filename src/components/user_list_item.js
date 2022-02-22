d3c1f23d142287205b570f72d2bda3afff95b34c
Matt Holton <mtholton@valdosta.edu>
2022-02-22 01:28:09 -0500

    added DoB, email, phone to user_list_item

--- src/components/user_list_item.js
+++ src/components/user_list_item.js
@@ -4,11 +4,24 @@ const UserListItem = ({ u, onUserSelect }) => {
 	const name = u.name.title + ". " + u.name.first + " " + u.name.last;
 
 	const nat = u.nat;
+
+	const gender = u.gender;
+
+	const DoB = u.DoB;
+
+	const email = u.email;
+
+	const phone = u.phone;
+
 	return (
 		<li onClick={() => onUserSelect(u)}>
 			<div>
 				<div>{name}</div>
+				<div>{gender}</div>
+				<div>{DoB}</div>
 				<div>{nat}</div>
+				<div>{email}</div>
+				<div>{phone}</div>
 			</div>
 		</li>
 	);
